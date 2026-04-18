10b0ec1b3895444d4268fb9d0fe7c3a5bc4c5277
mahmoud etman <metman835@gmail.com>
2026-04-18 02:38:22 +0200

    adding resnet as encoder for unet

---
+++ baseline_one/pretrained_model/resnetunet_model.py
@@ -0,0 +1,86 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision import models
+
+
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU()
+        )
+
+    def forward(self, x):
+        return self.conv(x)
+    
+
+
+class DecoderBlock(nn.Module):
+    def __init__(self, in_channels, skip_connections, out_channels):
+        super(DecoderBlock, self).__init__()
+        self.up_sampling = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv_block = DoubleConv(out_channels+skip_connections, out_channels)
+
+    def forward(self, x, skip=None):
+        x = self.up_sampling(x)
+
+        if skip is not None:
+            # Padding in case the input dimensions are not perfectly divisible by 2
+            diffY = skip.size()[2] - x.size()[2]
+            diffX = skip.size()[3] - x.size()[3]
+            
+            x = F.pad(x, [diffX // 2, diffX - diffX // 2,
+                          diffY // 2, diffY - diffY // 2])
+            
+            # Concatenate along the channel dimension
+            x = torch.cat([skip, x], dim=1)
+
+        return self.conv_block(x)
+    
+
+
+class ResNetUNet(nn.Module):
+    def __init__(self, num_classes):
+        super(ResNetUNet, self).__init__()
+        model = models.resnet34(weights='DEFAULT')
+
+        self.encoder0 = nn.Sequential(
+            model.conv1,
+            model.bn1,
+            model.relu
+        )
+        self.pool = model.maxpool
+        self.encoder1 = model.layer1
+        self.encoder2 = model.layer2
+        self.encoder3 = model.layer3
+        self.encoder4 = model.layer4
+
+        # Decoder
+        self.up4 = DecoderBlock(512, 256, 256)
+        self.up3 = DecoderBlock(256, 128, 128)
+        self.up2 = DecoderBlock(128,  64,  64)
+        self.up1 = DecoderBlock( 64,  64,  64)
+        self.up0 = DecoderBlock(64, 0, 64)
+
+        self.out_layer = nn.Conv2d(64, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        e0 = self.encoder0(x)
+        e1 = self.encoder1(self.pool(e0))
+        e2 = self.encoder2(e1)
+        e3 = self.encoder3(e2)
+        e4 = self.encoder4(e3) # Bottleneck layer
+
+        d = self.up4(e4, skip=e3)
+        d = self.up3(d, skip=e2)
+        d = self.up2(d, skip=e1)
+        d = self.up1(d, skip=e0)
+        d = self.up0(d)
+
+        return self.out_layer(d)
